a7b52803e22972bc4a83f6ed9852cb95d1dae69c
Devan Burke <vancroth@gmail.com>
2010-03-12 02:35:08 -0800

    whatever

--- include/PowerUp.h
+++ include/PowerUp.h
@@ -10,11 +10,14 @@ namespace FormFactor {
 class PowerUp : public Primitive
 {
 public: 
-	PowerUp(Ogre::SceneNode *node); 
+	PowerUp(Ogre::SceneNode *node, std::vector<Point> &points, float speed); 
 	virtual ~PowerUp(); 
 
-	virtual bool frameEvent(const Ogre::FrameEvent &evt);
-	virtual bool keyPressed(const OIS::KeyEvent &evt);
+	BoundingBox unmovingBound() const;
+
+	virtual bool frameEvent(const Ogre::FrameEvent &evt) {return true;}
+	virtual bool keyPressed(const OIS::KeyEvent &evt) {return true;}
+	virtual bool frameStarted(const FrameEvent& evt);
 
 	// Override Primitve functions
 	virtual BoundingBox worldBound() const;
@@ -23,6 +26,11 @@ public:
 
 protected:
 	Ogre::Entity *powerUp;
+	unsigned int dest;
+	Vector direction;
+	float distance, speed;
+	std::vector<Point> points;
+	BoundingBox movingBounds;
 
 private:
 	static unsigned int numPowerUps;
